0109df6e577f096c2537edb11b729de49c50531d
Siyu Yang <zozobar@bu.edu>
2026-09-18 12:00:36 -0400

    Add Discussion 2

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log('Happy developing ✨')
+let booleanVar = false;
+
+function boo() {
+    if (booleanVar === false) {
+        // display the sacary content
+        document.getElementById("scary-content").style.display = "block";
+        document.getElementById("cute-content").style.display = "none";
+        document.getElementById("toggleBtn").textContent = "🐱 Make It Cute Again 🐱";
+        booleanVar = true;
+    } else {
+        // display normal
+        document.getElementById("scary-content").style.display = "none";
+        document.getElementById("cute-content").style.display = "block";
+        document.getElementById("toggleBtn").textContent = "😈 Press If You Dare 😈";
+        booleanVar = false;
+    }
+}
